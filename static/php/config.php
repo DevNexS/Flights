bd9ec9f86dd55f05da2d73d306ffa131a2b248d3
unknown <ilarimsa937@gmail.com>
2022-02-06 16:33:49 +0200

    added folders

--- static/php/config.php
+++ static/php/config.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 $host = "localhost"; /* Host name */
-$user = "id16842393_admin"; /* User */
-$password = "RVT_1_Flights"; /* Password */
-$dbname = "id16842393_main"; /* Database name */
+$user = "name"; /* User */
+$password = "localhost"; /* Password */
+$dbname = "id16842393_admin"; /* Database name */
 
 $con = mysqli_connect($host, $user, $password,$dbname);
 // Check connection
