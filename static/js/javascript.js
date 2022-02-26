452191d86df7517a2a71029516c13b626839d515
cimpal55 <cimpals71@gmail.com>
2022-02-26 23:36:17 +0200

    minor changes

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -129,18 +129,24 @@ document.getElementById("ienakt").addEventListener("click", () => {
   popUp.style.display = "block";
 });
 
-let x = document.getElementById("login");
-let y = document.getElementById("register");
-let z = document.getElementById("btn");
-
 function register() {
-  x.style.left = "-400px";
-  y.style.left = "50px";
-  z.style.left = "100px";
-}
+  document.getElementById("login").style.left = "-400px";
+  document.getElementById("register").style.left = "50px";
+  document.getElementById("btn").style.left = "100px";
+  }
 
 function login() {
-  x.style.left = "50px";
-  y.style.left = "450px";
-  z.style.left = "0px";
+  document.getElementById("login").style.left = "50px";
+  document.getElementById("register").style.left = "450px";
+  document.getElementById("btn").style.left = "0px";
+}
+function est() {
+  document.getElementById("popUp").style.display = "block";
+}
+
+function net() {
+  document.getElementById("popUp").style.display = "none";
+}
+function swap(){
+  [document.getElementById("nokuriene").value, document.getElementById("wheretogo").value] = [document.getElementById("wheretogo").value, document.getElementById("nokuriene").value];
 }
