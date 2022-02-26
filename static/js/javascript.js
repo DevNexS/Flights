309b2d427518253285863bc856fd71a90be22709
unknown <ilarimsa937@gmail.com>
2022-02-26 22:34:36 +0200

    asd

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -123,3 +123,24 @@ switch (expr) {
   
   console.log("Is there anything else you'd like?");
 }
+
+document.getElementById("ienakt").addEventListener("click", () => {
+  let popUp = document.getElementById("popUp");
+  popUp.style.display = "block";
+});
+
+let x = document.getElementById("login");
+let y = document.getElementById("register");
+let z = document.getElementById("btn");
+
+function register() {
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "100px";
+}
+
+function login() {
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.style.left = "0px";
+}
